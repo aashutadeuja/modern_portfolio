83c4a08f8526b34b1d603e2e96872cdb72c56d80
LAPTOP-SPPJB5N9\aashu <aashuta@hotmail.com>
2023-01-16 13:43:39 +0200

    Index markup & main sass

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,4 +4,32 @@ const menuBtn = document.querySelector('.btn-menu');
 const menu = document.querySelector('.menu');
 const menuBranding = document.querySelector('menu.branding');
 const menuNav = document.selectQueries('.menu-nav');
-const navItems = document.queryselectorAll('.nav-items');
+const navItems = document.queryselectorAll('.nav-item');
+
+//Set initial state of menu
+let showMenu = false;
+menuBtn.eventListener('Click', toggleMenu);
+function toggleMenu() {
+    if(!showMenu){
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuBranding.classList.add('show');
+    menuNav.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+
+//Set Menu State
+showMenu = true;
+ }
+else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuBranding.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    //Set Menu State
+    showMenu = false;
+}
+
+}
