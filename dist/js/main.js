38ab9f2eb6338e5d79601f5e1e44e43c66ff0e1e
LAPTOP-SPPJB5N9\aashu <aashuta@hotmail.com>
2023-01-16 09:51:26 +0200

    Index markup & main sass

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,7 @@
 // Select DOM Items
 
 const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('menu.branding');
+const menuNav = document.selectQueries('.menu-nav');
+const navItems = document.queryselectorAll('.nav-items');
